f413bcc2d66103854e3265fba8e0d7b72994f872
Danak-UY <37196682+Danak-UY@users.noreply.github.com>
2020-12-09 23:30:39 -0300

    MELI Fonts

--- challenge-meli/src/components/CategoriesBreadcrumb.js
+++ challenge-meli/src/components/CategoriesBreadcrumb.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import "./../styles/categoriesBreadcrumb.css";
+
 function CategoriesBreadcrumb({ categories }) {
   return (
     <ul>
       {categories &&
-        categories.map((category) => {
-          return <li>{category}</li>;
+        categories.map((category, index) => {
+          return <li key={category + index}>{category}</li>;
         })}
     </ul>
   );
